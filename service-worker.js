846af2aa6689e017617955bf8aea04037fa817e4
Aspenini <bobfeltner123@gmail.com>
2024-10-17 22:58:34 -0400

    fixed IOS support

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,8 @@ const urlsToCache = [
     '/index.html',
     '/style.css',
     '/funky-icon.png',
+    '/img/funky-icon-192.png',
+    '/img/funky-icon-512.png',
     // Add more assets to cache here (e.g., game images, scripts, etc.)
 ];
 
@@ -15,36 +17,4 @@ self.addEventListener('install', event => {
         caches.open(CACHE_NAME)
             .then(cache => {
                 console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// Fetch cached content when offline
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;  // Return the cached file
-                }
-                return fetch(event.request);  // Fallback to network if not cached
-            })
-    );
-});
-
-// Activate the service worker and clean up old caches
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (cacheWhitelist.indexOf(key) === -1) {
-                        return caches.delete(key);  // Remove old caches
-                    }
-                })
-            );
-        })
-    );
-});
+                return cache.addAll(urlsToCache
